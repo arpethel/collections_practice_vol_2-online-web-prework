ff6ba8c01321c8479a9b4be808a21dbe179f437e
Ryan Pethel <arpethel@gmail.com>
2019-02-21 10:52:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,7 +22,7 @@ end
 def merge_data(h1, h2)
   h1.each do |data1|
     h2.each do |data2|
-      data1.merge!(data2[data1[:any_thing]])
+      data1.merge!(data2[data1[:first_name]])
     end
   end
 end
