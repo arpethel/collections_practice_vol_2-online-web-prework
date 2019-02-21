dc7953b1a8cfe6e2de3d64dc6a9ea2ebf1e11d84
Ryan Pethel <arpethel@gmail.com>
2019-02-21 10:45:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,6 +15,16 @@ def remove_non_strings(arr)
   arr.delete_if { |ele| ele != ele.to_s }
 end
 
-def count_elements(arr, ele)
-  arr.count(ele)
+# def count_elements(arr, ele)
+#   arr.count(ele)
+# end
+
+def merge_data(arr1, arr2)
+  my_arr = []
+
+  arr1.each do |ele1|
+    arr2.each do |ele2|
+      ele1.merge!(ele2[ele1[:some_thing]])
+    end
+  end
 end
