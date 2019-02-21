450caaa340c5c2e98296fb1369b402c11ab2fa52
Ryan Pethel <arpethel@gmail.com>
2019-02-21 10:16:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,19 @@
 # your code goes here
-def begins_with_r(tools)
-  tools.all? { |word| word.downcase.chr == "r"}
+def begins_with_r(arr)
+  arr.all? { |word| word.downcase.chr == "r" }
 end
+
+def contain_a(arr)
+  arr.select { |word| word.include?("a") }
+end
+
+def first_wa(arr)
+  arr.find { |word| word.to_s.start_with?("wa") }
+end
+
+def remove_non_strings(arr)
+  arr.delete_if { |ele| ele != ele.to_s }
+end
+
+def count_elements(arr)
+  arr.count { |ele| }
